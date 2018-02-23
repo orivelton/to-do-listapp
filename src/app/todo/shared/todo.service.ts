bfad2a1d30822e2eede38b32f2f9854534f627a9
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-02-23 19:05:30 -0300

    Add functions todo list and view

--- src/app/todo/shared/todo.service.ts
+++ src/app/todo/shared/todo.service.ts
@@ -6,4 +6,24 @@ export class TodoService {
   toDoList: AngularFireList<any>;
   constructor(private firebasedb: AngularFireDatabase) { }
 
+  getToDoList() {
+    this.toDoList = this.firebasedb.list('titles');
+    return this.toDoList;
+  }
+
+  addTitle(title: string) {
+    this.toDoList.push({
+      title: title,
+      isChecked: false
+    });
+  }
+
+  checkOrUnCheckTitle($key: string, flag: boolean) {
+    this.toDoList.update($key, {isChecked: flag});
+  }
+
+  removeTitle($key: string) {
+    this.toDoList.remove($key);
+  }
+
 }
