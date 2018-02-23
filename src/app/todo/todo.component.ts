fc4aa3d203fad23917a44ab1aa0344ecf3d5a130
Orivelton Cesar Da Conceição <oriveltoncesar@MacBook-Air-de-Orivelton.local>
2018-02-23 19:50:55 -0300

    includ function add and delete

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray: any[];
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+      //sort array isChecked false -> true
+      this.toDoListArray.sort((a,b) => {
+        return a.isChecked - b.isChecked;
+      })
+    });
+  }
+
+  onAdd(itemTitle) {
+    this.todoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
+
+  alterCheck($key: string, isChecked) {
+    this.todoService.checkOrUnCheckTitle($key, !isChecked);
+  }
+
+  onDelete($key: string) {
+    this.todoService.removeTitle($key);
   }
 
 }
